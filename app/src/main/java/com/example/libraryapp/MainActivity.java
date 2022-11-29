596010566830ba1fec8a5fc912cc4690b4d601f0
savan2073 <savan2073@gmail.com>
2022-11-29 15:43:31 +0100

    p13 done

--- app/src/main/java/com/example/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/libraryapp/MainActivity.java
@@ -4,19 +4,27 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.libraryapp.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,19 +38,19 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        //setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
+        /*
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
@@ -72,5 +80,61 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+
+    private class BookHolder extends RecyclerView.ViewHolder{
+
+        private TextView bookTitleTextView;
+        private TextView bookAuthorTextView;
+
+        public BookHolder(LayoutInflater inflater, ViewGroup parent){
+            super(inflater.inflate(R.layout.book_list_item, parent, false));
+
+            bookTitleTextView = itemView.findViewById(R.id.book_title_label);
+            bookAuthorTextView = itemView.findViewById(R.id.book_author_label);
+        }
+
+        public void bind(Book book){
+            bookTitleTextView.setText(book.getTitle());
+            bookAuthorTextView.setText(book.getAuthor());
+        }
+
+    }
+
+    private class BookAdapter extends RecyclerView.Adapter<BookHolder>{
+
+        private List<Book> books;
+
+        @NonNull
+        @Override
+        public BookHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new BookHolder(getLayoutInflater(), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull BookHolder holder, int position){
+            if(books != null){
+                Book book = books.get(position);
+                holder.bind(book);
+            }else{
+                Log.d("MainActivity", "No books");
+            }
+        }
+
+        @Override
+        public int getItemCount(){
+            if(books != null){
+                return books.size();
+            }else{
+                return 0;
+            }
+        }
+
+        public void setBooks(List<Book> books){
+            this.books = books;
+            notifyDataSetChanged();
+        }
+
     }
 }
